cbf2ee545b768894e00f4c65e3e7f23d4b9103f8
BennyLorenzo <bennylorenzo13@gmail.com>
2024-07-05 10:02:53 +0300

    Python Commit

---
+++ P01_intro/f02_kintamieji.py
@@ -0,0 +1,15 @@
+# python komentaras
+month = "sausis"
+day = "31"
+hour = 22
+
+print(month)
+print(day)
+print(hour)
+
+month_day = month + day
+
+print(month_day)
+print(month, day, hour, month_day)
+
+# CTRL + ALT + L = format code
